3a21afbecf3bd4acc0d7e76ca444bdb5afd3ab34
Nam Jun Lee <dlskawns96@gmail.com>
2023-05-17 17:33:41 +0900

    샘플앱 프로젝트 생성

---
+++ DealiDesignSystemSampleApp/DealiDesignSystemSampleApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DealiDesignSystemSampleApp
+//
+//  Created by 이남준 on 2023/05/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+

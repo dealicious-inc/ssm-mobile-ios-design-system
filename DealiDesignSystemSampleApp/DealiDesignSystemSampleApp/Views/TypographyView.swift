588a3ca5040e4f716052232d433a21de48998fd8
NamJunLee <dlskawns96@gmail.com>
2023-05-19 15:16:49 +0900

    폰트 샘플 뷰

--- DealiDesignSystemSampleApp/DealiDesignSystemSampleApp/Views/TypographyView.swift
+++ DealiDesignSystemSampleApp/DealiDesignSystemSampleApp/Views/TypographyView.swift
@@ -7,12 +7,17 @@
 
 import UIKit
 
+import FlexLayout
+import PinLayout
+
 import DealiDesignKit
 
 final class TypographyView: UIView {
     private let flexContainer = UIView()
     private let scrollView = UIScrollView()
     private let contentView = UIView()
+    
+    var fontViewArray: [FontView] = []
     
     init() {
         super.init(frame: .zero)
@@ -28,31 +33,19 @@ final class TypographyView: UIView {
         self.scrollView.flex.addItem(self.contentView)
         
         self.contentView.flex
-            .margin(12.0)
+            .padding(12.0)
+            .backgroundColor(.lightGray.withAlphaComponent(0.4))
             .define { flex in
                 DealiFont.allCases.forEach {
-                    flex.addItem(self.makeFontView(font: $0)).marginTop(10.0)
+                    flex.addItem(self.makeFontView(font: $0)).marginBottom(10.0)
                 }
             }
     }
     
     private func makeFontView(font: DealiFont) -> UIView {
-        let view = UIView()
-        view.flex.define {
-            
-            $0.addItem(UILabel().then {
-                $0.font = .b1Bold
-                $0.text = font.fontTitle
-            })
-            
-            $0.addItem(UILabel().then {
-                $0.font = font.font
-                $0.text = "HELLO WORLD hello world 안녕하세요"
-                $0.numberOfLines = 0
-            })
-        }
-        
-        return view
+        let fontView = FontView(font: font)
+        self.fontViewArray.append(fontView)
+        return fontView
     }
     
     override func layoutSubviews() {
@@ -63,6 +56,54 @@ final class TypographyView: UIView {
         
         self.scrollView.pin.all()
         self.scrollView.contentSize = self.contentView.frame.size
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+final class FontView: UIView {
+    private let flexContainer = UIView()
+    
+    let button = UIButton()
+    let label = UILabel()
+    
+    var isOpened = false
+    
+    init(font: DealiFont) {
+        super.init(frame: .zero)
+        
+        self.addSubview(self.flexContainer)
+        self.flexContainer.flex
+            .direction(.column)
+            .padding(10.0)
+            .backgroundColor(.white)
+            .define {
+                $0.addItem(button.then {
+                    $0.titleLabel?.font = font.font
+                    $0.contentHorizontalAlignment = .left
+                    $0.setTitle(font.fontTitle, for: .normal)
+                    $0.setTitleColor(.black, for: .normal)
+                })
+                
+                $0.addItem(label.then {
+                    $0.font = font.font
+                    $0.text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam non scelerisque nulla. Sed eu vulputate nibh, in posuere diam. Nullam."
+                    $0.textColor = .black
+                    $0.numberOfLines = 0
+                })
+                .height(0)
+            }
+    }
+    
+    
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        self.flexContainer.pin.all()
+        self.flexContainer.flex.layout()
     }
     
     required init?(coder: NSCoder) {

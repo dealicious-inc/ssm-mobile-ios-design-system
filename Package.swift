7d7376c6e3f917274d7d7a891eacc409735e8769
jalynneYoon <jalynneyoon@gmail.com>
2023-06-16 14:24:28 +0900

    test - RxCocoa 제거

--- Package.swift
+++ Package.swift
@@ -27,7 +27,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "DealiDesignKit",
-            dependencies: ["SnapKit", "Then", "FlexLayout", "PinLayout", "RxSwift", .product(name: "RxCocoa", package: "RxSwift")],
+            dependencies: ["SnapKit", "Then", "FlexLayout", "PinLayout", "RxSwift"], //, .product(name: "RxCocoa", package: "RxSwift")],
             resources: [.process("Resources/PretendardFonts")]
             ),
         

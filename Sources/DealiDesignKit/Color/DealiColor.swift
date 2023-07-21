cd431b942f25b21170914862b0f35c5af7f3e53e
NamJunLee <dlskawns96@gmail.com>
2023-07-21 17:13:02 +0900

    etc01 컬러 알파 값 변경

--- Sources/DealiDesignKit/Color/DealiColor.swift
+++ Sources/DealiDesignKit/Color/DealiColor.swift
@@ -156,7 +156,7 @@ enum EtcColor: Int, ColorConfigurable {
     var alpha: Double {
         switch self {
         case .etc01:
-            return 0.2
+            return 0.1
         case .etc02:
             return 0.2
         case .etc03:

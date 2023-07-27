67185158f10a38ccd45b396726c62c3ff5afda08
LeeChangHo <hoji0305@gmail.com>
2023-07-27 15:06:36 +0900

    DealiButton 이슈 수정중

--- Sources/DealiDesignKit/Components/Buttons/DealiButton.swift
+++ Sources/DealiDesignKit/Components/Buttons/DealiButton.swift
@@ -41,36 +41,65 @@ public class DealiButton: UIButton {
         }
     }
     
+    public var leftIconImage: UIImage? {
+        didSet {
+            self.setAppearance()
+        }
+    }
+    
+    public var rightIconImage: UIImage? {
+        didSet {
+            self.setAppearance()
+        }
+    }
+    
     public init(_ style: DealiButtonStyle = .large(style: .filled), title: String? = "") {
         super.init(frame: .zero)
         self.style = style
         self.title = title
         
-        self.layer.cornerRadius = 6.0
-        self.layer.masksToBounds = true
-        
         self.setTitle(title, for: .normal)
-       
+        
         self.setAppearance()
     }
     
     required init?(coder: NSCoder) {
         super.init(coder: coder)
+        
+        self.setAppearance()
     }
     
     func setAppearance() {
-        self.backgroundColor = self.style.defaultBackgroundColor
+
+        self.setBackgroundColor(self.style.defaultBackgroundColor, for: .normal)
+        self.setBackgroundColor(self.style.pressedBackgroundColor, for: .highlighted)
+        self.setBackgroundColor(self.style.disabledBackgroundColor, for: .disabled)
+
+        self.layer.cornerRadius = 6.0
+        self.layer.masksToBounds = true
         
         let horizontalPadding = self.style.padding.horizontal
         let verticalPadding = self.style.padding.vertical
-        
+
         let internalSpacing = self.style.padding.internalSpacing
-        
-        let halfInternalSpacing = internalSpacing / 2
-        self.contentEdgeInsets = .init(top: verticalPadding, left: horizontalPadding + halfInternalSpacing, bottom: verticalPadding, right: halfInternalSpacing + horizontalPadding)
-        self.imageEdgeInsets = .init(top: 0, left: -halfInternalSpacing, bottom: 0, right: halfInternalSpacing)
-        self.titleEdgeInsets = .init(top: 0, left: halfInternalSpacing, bottom: 0, right: -halfInternalSpacing)
-        
+
+        self.contentEdgeInsets = .init(top: verticalPadding, left: horizontalPadding, bottom: verticalPadding, right: horizontalPadding)
+        if let leftIconImage = self.leftIconImage {
+            self.imageEdgeInsets = .init(top: 0.0, left: -internalSpacing, bottom: 0.0, right: internalSpacing)
+
+            self.setImage(leftIconImage.withTintColor(self.style.textColor), for: .normal)
+            self.setImage(leftIconImage.withTintColor(self.style.pressedTextColor), for: .highlighted)
+            self.setImage(leftIconImage.withTintColor(self.style.disabledTextColor), for: .selected)
+            self.semanticContentAttribute = .forceLeftToRight
+        } else if let rightIconImage = self.rightIconImage {
+            self.imageEdgeInsets = .init(top: 0.0, left: -internalSpacing, bottom: 0.0, right: internalSpacing)
+
+            self.setImage(rightIconImage.withTintColor(self.style.textColor), for: .normal)
+            self.setImage(rightIconImage.withTintColor(self.style.pressedTextColor), for: .highlighted)
+            self.setImage(rightIconImage.withTintColor(self.style.disabledTextColor), for: .disabled)
+            self.semanticContentAttribute = .forceRightToLeft
+        }
+
         
         if self.style.hasBorder {
             self.layer.borderColor = self.style.defaultBorderColor
@@ -79,44 +108,60 @@ public class DealiButton: UIButton {
         
         self.setTitleColor(self.style.textColor, for: .normal)
         self.setTitleColor(self.style.pressedTextColor, for: .highlighted)
-        self.setTitleColor(self.style.disabledTextColor, for: .disabled)
+        self.setTitleColor(self.style.disabledTextColor, for: .selected)
         self.titleLabel?.font = self.style.font
+        
     }
     
-    public override var isHighlighted: Bool {
-        get {
-            return super.isHighlighted
-        }
-        set {
-            if newValue {
-                backgroundColor = self.style.pressedBackgroundColor
-            } else {
-                backgroundColor = self.isEnabled ? self.style.defaultBackgroundColor : self.style.disabledBackgroundColor
-            }
-            super.isHighlighted = newValue
-        }
-    }
+//    public override var isHighlighted: Bool {
+//        get {
+//            return super.isHighlighted
+//        }
+//        set {
+//            if newValue {
+//                backgroundColor = self.style.pressedBackgroundColor
+//            } else {
+//                backgroundColor = self.isEnabled ? self.style.defaultBackgroundColor : self.style.disabledBackgroundColor
+//            }
+//            super.isHighlighted = newValue
+//        }
+//    }
     
-    public override var isEnabled: Bool {
-        get {
-            return super.isEnabled
-        }
-        set {
-            self.isUserInteractionEnabled = isEnabled
+//    public override var isEnabled: Bool {
+//        get {
+//            return super.isEnabled
+//        }
+//        set {
+//            self.isUserInteractionEnabled = isEnabled
+//
+//            if newValue == false {
+////                backgroundColor =  self.style.disabledBackgroundColor
+////                self.setTitleColor(self.style.disabledTextColor, for: .normal)
+//
+//                if self.style.hasBorder {
+//                    self.layer.borderColor = self.style.disabledBorderColor
+//                    self.layer.borderWidth = 1.0
+//                }
+//            }
+//
+//            super.isEnabled = newValue
+//        }
+//    }
+    
+}
+
+extension DealiButton {
+    func setBackgroundColor(_ color: UIColor, for state: UIControl.State) {
+            UIGraphicsBeginImageContext(CGSize(width: 1.0, height: 1.0))
+            guard let context = UIGraphicsGetCurrentContext() else { return }
+            context.setFillColor(color.cgColor)
+            context.fill(CGRect(x: 0.0, y: 0.0, width: 1.0, height: 1.0))
             
-            if newValue == false {
-                backgroundColor =  self.style.disabledBackgroundColor
-                self.setTitleColor(self.style.disabledTextColor, for: .normal)
-                
-                if self.style.hasBorder {
-                    self.layer.borderColor = self.style.disabledBorderColor
-                    self.layer.borderWidth = 1.0
-                }
-            }
-            
-            super.isEnabled = newValue
+            let backgroundImage = UIGraphicsGetImageFromCurrentImageContext()
+            UIGraphicsEndImageContext()
+             
+            self.setBackgroundImage(backgroundImage, for: state)
         }
-    }
 }
 
 #if canImport(SwiftUI) && DEBUG
@@ -132,7 +177,7 @@ struct ButtonPreview: PreviewProvider {
                 Group {
                     Text("Large")
                         .font(.system(size: 20.0, weight: .bold))
-
+                    
                     buttonStyle(style: .large(style: .filled))
                     buttonStyle(style: .large(style: .outlined))
                     buttonStyle(style: .large(style: .tonal))
@@ -141,7 +186,7 @@ struct ButtonPreview: PreviewProvider {
                 Group {
                     Text("Medium")
                         .font(.system(size: 20.0, weight: .bold))
-
+                    
                     buttonStyle(style: .medium(style: .filled))
                     buttonStyle(style: .medium(style: .outlined))
                     buttonStyle(style: .medium(style: .tonal))
@@ -150,7 +195,7 @@ struct ButtonPreview: PreviewProvider {
                 Group {
                     Text("Small")
                         .font(.system(size: 20.0, weight: .bold))
-
+                    
                     buttonStyle(style: .small(style: .filled))
                     buttonStyle(style: .small(style: .outlined))
                     buttonStyle(style: .small(style: .tonal))
@@ -162,29 +207,52 @@ struct ButtonPreview: PreviewProvider {
         .previewDisplayName("DealiButtonStyle")
     }
     
-
+    
     @ViewBuilder static func buttonStyle(style: DealiButtonStyle) -> some View {
         VStack(alignment: .leading) {
             Text(String(describing: style))
-                UIViewPreview {
-                    let button = DealiButton(title: "Default 버튼")
-                    button.style = style
-                    return button
-                }
-                
-                UIViewPreview {
-                    let button = DealiButton(title: "Pressed 버튼")
-                    button.style = style
-                    button.isHighlighted = true
-                    return button
-                }
-                
-                UIViewPreview {
-                    let button = DealiButton(title: "Disabled 버튼")
-                    button.style = style
-                    button.isEnabled = false
-                    return button
-                }
+            UIViewPreview {
+                let button = DealiButton(title: "Default 버튼")
+                button.style = style
+                return button
+            }
+            
+            UIViewPreview {
+                let button = DealiButton(title: "Pressed 버튼")
+                button.style = style
+                button.isHighlighted = true
+                return button
+            }
+            
+            UIViewPreview {
+                let button = DealiButton(title: "Disabled 버튼")
+                button.style = style
+                button.isEnabled = false
+                return button
+            }
+            
+            UIViewPreview {
+                let button = DealiButton(title: "Default 버튼")
+                button.style = style
+                button.leftIconImage = UIImage(named: "ic_arrow_left_16", in: Bundle.module, compatibleWith: nil)
+                return button
+            }
+            
+            UIViewPreview {
+                let button = DealiButton(title: "Pressed 버튼")
+                button.style = style
+                button.leftIconImage = UIImage(named: "ic_arrow_left_16", in: Bundle.module, compatibleWith: nil)
+                button.isHighlighted = true
+                return button
+            }
+            
+            UIViewPreview {
+                let button = DealiButton(title: "Disabled 버튼")
+                button.style = style
+                button.rightIconImage = UIImage(named: "ic_arrow_right_16", in: Bundle.module, compatibleWith: nil)
+                button.isEnabled = false
+                return button
+            }
         }
     }
 }

493e866e1be17e211fbcfed8f14406d512780212
LeeChangHo <hoji0305@gmail.com>
2023-08-10 18:30:04 +0900

    DealiAlert 기본 스타일 구현

--- Sources/DealiDesignKit/Components/Alert/DealiAlertViewController.swift
+++ Sources/DealiDesignKit/Components/Alert/DealiAlertViewController.swift
@@ -8,17 +8,93 @@
 import UIKit
 
 final class DealiAlert: NSObject {
-    
+    class func show(title: String? = nil, message: String, cancelButtonTitle: String? = nil, confirmButtonTitle: String? = nil, closeAlertOnOutsideTouch: Bool = true, cancelAndCloseOnOutsideTouch: Bool = false, alertPresentingViewController: UIViewController, cancelAction: (() -> Swift.Void)?, confirmAction: (() -> Swift.Void)?) {
+        
+        let style = NSMutableParagraphStyle()
+        style.lineSpacing = 4.0
+        style.alignment = .left
+        
+        let atMessage = NSMutableAttributedString(string: message, attributes: [.font: UIFont.sh3r16, .foregroundColor: DealiColor.text03, .paragraphStyle: style])
+        
+        
+        let alertViewController = DealiAlertViewController()
+        alertViewController.alertTitle = title
+        alertViewController.alertMessage = atMessage
+        alertViewController.cancelButtonTitle = cancelButtonTitle
+        alertViewController.confirmButtonTitle = confirmButtonTitle
+        alertViewController.closeAlertOnOutsideTouch = closeAlertOnOutsideTouch
+        alertViewController.cancelAndCloseOnOutsideTouch = cancelAndCloseOnOutsideTouch
+        alertViewController.cancelAction = cancelAction
+        alertViewController.confirmAction = confirmAction
+        
+        alertPresentingViewController.present(alertViewController, animated: true)
+    }
 }
 
 final class DealiAlertViewController: UIViewController {
 
     private let contentView = UIView()
+    private let contentStackView = UIStackView()
     private let titleLabel = UILabel()
     private let messageLabel = UILabel()
     
     private let cancelButton = DealiButton()
     private let confirmButton = DealiButton()
+    
+    var cancelAction: (() -> Swift.Void)?
+    var confirmAction: (() -> Swift.Void)?
+    
+    var alertTitle: String? {
+        didSet {
+            if let title = self.alertTitle {
+                self.titleLabel.isHidden = false
+                self.titleLabel.text = title
+                
+                self.contentStackView.snp.updateConstraints {
+                    $0.top.equalToSuperview().offset(24.0)
+                }
+            } else {
+                self.titleLabel.isHidden = true
+                self.contentStackView.snp.updateConstraints {
+                    $0.top.equalToSuperview().offset(28.0)
+                }
+            }
+        }
+    }
+    
+    var alertMessage: NSMutableAttributedString? {
+        didSet {
+            if let message = self.alertMessage {
+                self.messageLabel.attributedText = message
+            }
+        }
+    }
+    
+    var cancelButtonTitle: String? {
+        didSet {
+            if let buttontitle = self.cancelButtonTitle {
+                self.cancelButton.isHidden = false
+                self.cancelButton.title = buttontitle
+            } else {
+                self.cancelButton.isHidden = true
+            }
+        }
+    }
+    
+    var confirmButtonTitle: String? {
+        didSet {
+            if let buttontitle = self.confirmButtonTitle {
+                self.confirmButton.isHidden = false
+                self.confirmButton.title = buttontitle
+            } else {
+                self.confirmButton.isHidden = true
+            }
+        }
+    }
+    /// content 영역 이외의 영역 터치시 alert닫기
+    var closeAlertOnOutsideTouch: Bool = false
+    /// content 영역 이외의 영턱 터치시 cancelAction 실행과 alert닫기
+    var cancelAndCloseOnOutsideTouch: Bool = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -40,9 +116,8 @@ final class DealiAlertViewController: UIViewController {
             $0.height.lessThanOrEqualTo(456.0)
         }
         
-        let contentStackView = UIStackView()
-        self.contentView.addSubview(contentStackView)
-        contentStackView.then {
+        self.contentView.addSubview(self.contentStackView)
+        self.contentStackView.then {
             $0.axis = .vertical
             $0.alignment = .fill
             $0.distribution = .fill
@@ -52,21 +127,20 @@ final class DealiAlertViewController: UIViewController {
             $0.left.bottom.right.equalToSuperview().inset(20.0)
         }
         
-        contentStackView.addArrangedSubview(self.titleLabel)
+        self.contentStackView.addArrangedSubview(self.titleLabel)
         self.titleLabel.then {
             $0.font = .sh1sb20
             $0.textColor = DealiColor.text01
             $0.textAlignment = .left
             $0.numberOfLines = 0
             $0.text = "Temp Alert Title"
-//            $0.isHidden = true
         }.snp.makeConstraints {
             $0.left.right.equalToSuperview()
         }
         
-        contentStackView.setCustomSpacing(20.0, after: self.titleLabel)
+        self.contentStackView.setCustomSpacing(20.0, after: self.titleLabel)
         
-        contentStackView.addArrangedSubview(self.messageLabel)
+        self.contentStackView.addArrangedSubview(self.messageLabel)
         self.messageLabel.then {
             $0.font = .sh3r16
             $0.textColor = DealiColor.text03
@@ -78,7 +152,7 @@ final class DealiAlertViewController: UIViewController {
         }
         
         let buttonStackView = UIStackView()
-        contentStackView.addArrangedSubview(buttonStackView)
+        self.contentStackView.addArrangedSubview(buttonStackView)
         buttonStackView.then {
             $0.axis = .horizontal
             $0.alignment = .fill
@@ -92,6 +166,7 @@ final class DealiAlertViewController: UIViewController {
         self.cancelButton.then {
             $0.style = .medium(style: .outlined)
             $0.title = "취소"
+            $0.addTarget(self, action: #selector(cancelButtonAction), for: .touchUpInside)
         }.snp.makeConstraints {
             $0.top.bottom.equalToSuperview()
         }
@@ -100,11 +175,39 @@ final class DealiAlertViewController: UIViewController {
         self.confirmButton.then {
             $0.style = .medium(style: .filled)
             $0.title = "확인"
+            $0.addTarget(self, action: #selector(confirmButtonAction), for: .touchUpInside)
         }.snp.makeConstraints {
             $0.top.bottom.equalToSuperview()
         }
     }
 
+    @objc func cancelButtonAction() {
+        self.dismiss(animated: true) { [weak self] in
+            if let self = self, let action = self.cancelAction {
+                action()
+            }
+        }
+    }
+    
+    @objc func confirmButtonAction() {
+        self.dismiss(animated: true) { [weak self] in
+            if let self = self, let action = self.confirmAction {
+                action()
+            }
+        }
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesEnded(touches, with: event)
+        guard let touch = touches.first, self.contentView.bounds.contains(touch.location(in: self.contentView)) == false  else { return }
+        if self.closeAlertOnOutsideTouch == true {
+            self.dismiss(animated: true, completion: nil)
+        } else {
+            if self.cancelAndCloseOnOutsideTouch == true {
+                self.cancelButtonAction()
+            }
+        }
+    }
 }
 
 extension UIViewController {

9483173d475df39dc1e4b82c087cf6dc580d2103
LeeChangHo <hoji0305@gmail.com>
2023-08-14 18:11:08 +0900

    Alert 새로 만들기

---
+++ DealiDesignSystemSampleApp/DealiDesignSystemSampleApp/ViewControllers/AlertTestViewController.swift
@@ -0,0 +1,67 @@
+//
+//  AlertTestViewController.swift
+//  DealiDesignSystemSampleApp
+//
+//  Created by hoji on 2023/08/14.
+//
+
+import UIKit
+import DealiDesignKit
+
+class AlertTestViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.title = "Alert"
+        self.view.backgroundColor = .white
+    }
+
+    override func loadView() {
+        super.loadView()
+        
+        let contentScrollView = UIScrollView()
+        self.view.addSubview(contentScrollView)
+        contentScrollView.then {
+            $0.bounces = false
+        }.snp.makeConstraints {
+            $0.top.bottom.left.right.equalToSuperview()
+        }
+        
+        let contentView = UIView()
+        contentScrollView.addSubview(contentView)
+        contentView.then {
+            $0.backgroundColor = .clear
+        }.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+            $0.width.equalToSuperview()
+        }
+        
+        let contentStackView = UIStackView()
+        contentView.addSubview(contentStackView)
+        contentStackView.then {
+            $0.axis = .vertical
+            $0.spacing = 10.0
+            $0.alignment = .center
+            $0.distribution = .equalSpacing
+        }.snp.makeConstraints {
+            $0.top.bottom.left.right.equalToSuperview().inset(20.0)
+        }
+        
+        let alertButton01 = DealiButton()
+        contentStackView.addArrangedSubview(alertButton01)
+        alertButton01.then {
+            $0.style = .large(style: .filled)
+            $0.title = "alert01"
+            $0.addTarget(self, action: #selector(alertButton01Pressed), for: .touchUpInside)
+        }.snp.makeConstraints {
+            $0.left.right.equalToSuperview()
+        }
+    }
+}
+
+extension AlertTestViewController {
+    @objc func alertButton01Pressed() {
+        print("alertButton01Pressed")
+    }
+}
---
+++ Sources/DealiDesignKit/Components/Alert/DealiAlert.swift
@@ -0,0 +1,17 @@
+//
+//  File.swift
+//  
+//
+//  Created by hoji on 2023/08/14.
+//
+
+import Foundation
+import UIKit
+
+final class DealiAlert: NSObject {
+    
+}
+
+final class DealiAlertViewController: UIViewController {
+    
+}

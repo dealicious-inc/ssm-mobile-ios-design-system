60a7d0203c9c3620c148115307c72f302db54461
jalynneyoon <jalynneyoon@gmail.com>
2023-11-06 17:12:55 +0900

    bottomSheet VC loadView 접근제한자 변경

--- Sources/DealiDesignKit/Components/BottomSheet/DealiBottomSheetViewController.swift
+++ Sources/DealiDesignKit/Components/BottomSheet/DealiBottomSheetViewController.swift
@@ -28,7 +28,7 @@ open class DealiBottomSheetViewController: UIViewController {
     
     private var cornerLayer: CAShapeLayer?
 
-    override public func loadView() {
+    override open func loadView() {
         super.loadView()
         
         self.do {
@@ -143,7 +143,7 @@ open class DealiBottomSheetViewController: UIViewController {
         }
     }
     
-    required init?(coder: NSCoder) {
+    required public init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 }

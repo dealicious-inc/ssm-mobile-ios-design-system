b981b299e67182a9398e71ca7f692316da44d72d
Johyeon Yoon <jalynneyoon@gmail.com>
2024-02-26 11:28:00 +0900

    [MP-4263] 에러문구 수정

--- Sources/DealiDesignKit/Utils/Extensions/UIImage+Extension.swift
+++ Sources/DealiDesignKit/Utils/Extensions/UIImage+Extension.swift
@@ -10,7 +10,7 @@ import UIKit
 public extension UIImage {
     static func dealiIcon(_ name: String) -> UIImage? {
         guard let image = UIImage(named: name, in: Bundle.module, compatibleWith: nil) else {
-            fatalError("디자인시스템에 정의된 아이콘이 아닙니다.")
+            fatalError("\(name) 은 디자인시스템에 정의된 아이콘이 아닙니다.")
         }
         
         return image

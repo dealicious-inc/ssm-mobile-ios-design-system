51f54f7477a0d6c59afde53dd552e1f0fc6927f9
jalynneyoon <jalynneyoon@gmail.com>
2024-03-13 15:14:31 +0900

    PR 리뷰 반영

--- Sources/DealiDesignKit/Utils/Extensions/UIImage+Extension.swift
+++ Sources/DealiDesignKit/Utils/Extensions/UIImage+Extension.swift
@@ -9,9 +9,8 @@ import UIKit
 
 public extension UIImage {
     static func dealiIcon(_ name: String) -> UIImage? {
-        guard let image = UIImage(named: name, in: Bundle.module, compatibleWith: nil) else {
-            fatalError("디자인시스템에 정의된 아이콘이 아닙니다.")
-        }
+        let image = UIImage(named: name, in: Bundle.module, compatibleWith: nil)
+        assert(image != nil, "디자인시스템에 정의된 아이콘이 아닙니다." )
         
         return image
     }

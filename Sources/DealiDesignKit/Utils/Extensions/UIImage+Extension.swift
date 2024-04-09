c934cf14ef1e5e3744bd9f7176feb985e0700b16
Johyeon Yoon <jalynneyoon@gmail.com>
2024-04-09 11:27:21 +0900

    DealiTextInput 아이콘 사이즈 변경

--- Sources/DealiDesignKit/Utils/Extensions/UIImage+Extension.swift
+++ Sources/DealiDesignKit/Utils/Extensions/UIImage+Extension.swift
@@ -14,4 +14,10 @@ public extension UIImage {
         
         return image
     }
+    
+    func resize(_ size: CGSize) -> UIImage {
+        return UIGraphicsImageRenderer(size: size).image { _ in
+            draw(in: CGRect(origin: .zero, size: size))
+        }
+    }
 }

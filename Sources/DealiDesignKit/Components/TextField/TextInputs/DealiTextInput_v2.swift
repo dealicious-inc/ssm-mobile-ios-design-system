3ec87128e3dbcfbd89524424c9b48b4e23e74bf6
jalynneyoon <jalynneyoon@gmail.com>
2024-05-26 21:19:10 +0900

    TextInput final 제거

--- Sources/DealiDesignKit/Components/TextField/TextInputs/DealiTextInput_v2.swift
+++ Sources/DealiDesignKit/Components/TextField/TextInputs/DealiTextInput_v2.swift
@@ -11,7 +11,7 @@ import SnapKit
 import RxSwift
 import RxCocoa
 
-public final class DealiTextInput_v2: UIView, DealiTextField {
+public class DealiTextInput_v2: UIView, DealiTextField {
     
     public private(set) var textField = UITextField()
 

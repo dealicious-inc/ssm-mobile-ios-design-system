88611e0b450be3f4d0fe9e4095331ea29cae2845
이창호 <ichangho@hoji.local>
2024-12-03 19:42:25 +0900

    [MP-5173] TabBar layout이슈 수정

--- Sources/DealiDesignKit/Components/TabBar/DealiTabBarView.swift
+++ Sources/DealiDesignKit/Components/TabBar/DealiTabBarView.swift
@@ -90,6 +90,7 @@ final public class DealiTabBarView: UIView {
                 layout.do {
                     $0.scrollDirection = .horizontal
                     $0.minimumInteritemSpacing = self.preset.itemSpacing
+                    $0.minimumLineSpacing = 0.0
                     $0.sectionInset = UIEdgeInsets.init(top: 0.0, left: self.preset.tabBarHorizontalMargin, bottom: 0.0, right: self.preset.tabBarHorizontalMargin)
                 }
             }

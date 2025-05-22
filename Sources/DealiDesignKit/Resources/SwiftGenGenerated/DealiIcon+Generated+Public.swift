fb03ec0585732cf60d545d91bd2e23dfe90ebe60
조서현 <chosh@deali.net>
2025-05-22 16:59:09 +0900

    [MP-5709] 디자인시스템 SwiftGen 추가 중, 중복 리소스 정리

---
+++ Sources/DealiDesignKit/Resources/SwiftGenGenerated/DealiIcon+Generated+Public.swift
@@ -0,0 +1,26 @@
+// swiftlint:disable all
+// Generated using SwiftGen — https://github.com/SwiftGen/SwiftGen
+
+  import UIKit
+#if canImport(SwiftUI)
+  import SwiftUI
+#endif
+
+// swiftlint:disable superfluous_disable_command file_length implicit_return
+
+// MARK: - Asset Catalogs
+
+// swiftlint:disable identifier_name line_length nesting type_body_length type_name
+extension DealiIcon {
+  static let ic_error_filled_24_ver01 = DesignSystemImage(name: "ic_error_filled_24_ver01")
+  static let img_laundry_bleach = DesignSystemImage(name: "img_laundry_bleach")
+  static let img_laundry_dry = DesignSystemImage(name: "img_laundry_dry")
+  static let img_laundry_handwasher = DesignSystemImage(name: "img_laundry_handwasher")
+  static let img_laundry_iron = DesignSystemImage(name: "img_laundry_iron")
+  static let img_laundry_nowasher = DesignSystemImage(name: "img_laundry_nowasher")
+  static let img_laundry_single = DesignSystemImage(name: "img_laundry_single")
+  static let img_laundry_washer = DesignSystemImage(name: "img_laundry_washer")
+  static let img_laundry_water = DesignSystemImage(name: "img_laundry_water")
+  static let img_tailanchor = DesignSystemImage(name: "img_tailanchor")
+}
+// swiftlint:enable identifier_name line_length nesting type_body_length type_name
---
+++ Sources/DealiDesignKit/Resources/SwiftGenGenerated/DealiIcon+Generated.swift
@@ -0,0 +1,322 @@
+// swiftlint:disable all
+// Generated using SwiftGen — https://github.com/SwiftGen/SwiftGen
+
+  import UIKit
+#if canImport(SwiftUI)
+  import SwiftUI
+#endif
+
+// swiftlint:disable superfluous_disable_command file_length implicit_return
+
+// MARK: - Asset Catalogs
+
+// swiftlint:disable identifier_name line_length nesting type_body_length type_name
+internal enum DealiIcon {
+  static let ic_dresses = DesignSystemImage(name: "ic_dresses")
+  static let ic_earrings = DesignSystemImage(name: "ic_earrings")
+  static let ic_kidsclothes = DesignSystemImage(name: "ic_kidsclothes")
+  static let ic_kidshats = DesignSystemImage(name: "ic_kidshats")
+  static let ic_menbags = DesignSystemImage(name: "ic_menbags")
+  static let ic_menshoes = DesignSystemImage(name: "ic_menshoes")
+  static let ic_tsharts = DesignSystemImage(name: "ic_tsharts")
+  static let ic_watches = DesignSystemImage(name: "ic_watches")
+  static let ic_womenacc = DesignSystemImage(name: "ic_womenacc")
+  static let ic_womenbags = DesignSystemImage(name: "ic_womenbags")
+  static let ic_womenshoes = DesignSystemImage(name: "ic_womenshoes")
+  static let ic_checkbox_disable = DesignSystemImage(name: "ic_checkbox_disable")
+  static let ic_checkbox_off = DesignSystemImage(name: "ic_checkbox_off")
+  static let ic_checkbox_on = DesignSystemImage(name: "ic_checkbox_on")
+  static let ic_checkbox_ondisable = DesignSystemImage(name: "ic_checkbox_ondisable")
+  static let ic_checkcircle_disable = DesignSystemImage(name: "ic_checkcircle_disable")
+  static let ic_checkcircle_off = DesignSystemImage(name: "ic_checkcircle_off")
+  static let ic_checkcircle_on = DesignSystemImage(name: "ic_checkcircle_on")
+  static let ic_checkcircle_onad = DesignSystemImage(name: "ic_checkcircle_onad")
+  static let ic_checkline_disabled = DesignSystemImage(name: "ic_checkline_disabled")
+  static let ic_checkline_off = DesignSystemImage(name: "ic_checkline_off")
+  static let ic_checkline_on = DesignSystemImage(name: "ic_checkline_on")
+  static let ic_checkline_onad = DesignSystemImage(name: "ic_checkline_onad")
+  static let ic_email = DesignSystemImage(name: "ic_email")
+  static let ic_empty40 = DesignSystemImage(name: "ic_empty40")
+  static let ic_expander_filled = DesignSystemImage(name: "ic_expander_filled")
+  static let ic_flight_filled = DesignSystemImage(name: "ic_flight_filled")
+  static let ic_imgsearch = DesignSystemImage(name: "ic_imgsearch")
+  static let ic_kakaotalk = DesignSystemImage(name: "ic_kakaotalk")
+  static let ic_line = DesignSystemImage(name: "ic_line")
+  static let ic_qrcode = DesignSystemImage(name: "ic_qrcode")
+  static let ic_radiobutton_disable = DesignSystemImage(name: "ic_radiobutton_disable")
+  static let ic_radiobutton_off = DesignSystemImage(name: "ic_radiobutton_off")
+  static let ic_radiobutton_on = DesignSystemImage(name: "ic_radiobutton_on")
+  static let ic_ssmall = DesignSystemImage(name: "ic_ssmall")
+  static let ic_staralpha = DesignSystemImage(name: "ic_staralpha")
+  static let ic_wechat = DesignSystemImage(name: "ic_wechat")
+  static let ic_alarm_ad_filled = DesignSystemImage(name: "ic_alarm_ad_filled")
+  static let ic_alarm_announce_filled = DesignSystemImage(name: "ic_alarm_announce_filled")
+  static let ic_alarm_etc_filled = DesignSystemImage(name: "ic_alarm_etc_filled")
+  static let ic_alarm_filled = DesignSystemImage(name: "ic_alarm_filled")
+  static let ic_alarm_order_filled = DesignSystemImage(name: "ic_alarm_order_filled")
+  static let ic_alarm_request_filled = DesignSystemImage(name: "ic_alarm_request_filled")
+  static let ic_arrow_close_1_filled = DesignSystemImage(name: "ic_arrow_close_1_filled")
+  static let ic_arrow_open_1_filled = DesignSystemImage(name: "ic_arrow_open_1_filled")
+  static let ic_bookmark_1_filled = DesignSystemImage(name: "ic_bookmark_1_filled")
+  static let ic_calendar_filled = DesignSystemImage(name: "ic_calendar_filled")
+  static let ic_camera_filled = DesignSystemImage(name: "ic_camera_filled")
+  static let ic_cart_filled = DesignSystemImage(name: "ic_cart_filled")
+  static let ic_cash_filled = DesignSystemImage(name: "ic_cash_filled")
+  static let ic_category_1_filled = DesignSystemImage(name: "ic_category_1_filled")
+  static let ic_category_filled = DesignSystemImage(name: "ic_category_filled")
+  static let ic_checkcircle_filled = DesignSystemImage(name: "ic_checkcircle_filled")
+  static let ic_checkcircle_no1_filled = DesignSystemImage(name: "ic_checkcircle_no1_filled")
+  static let ic_checkcircle_no2_filled = DesignSystemImage(name: "ic_checkcircle_no2_filled")
+  static let ic_checkcircle_no3_filled = DesignSystemImage(name: "ic_checkcircle_no3_filled")
+  static let ic_deliverybox_filled = DesignSystemImage(name: "ic_deliverybox_filled")
+  static let ic_download_1_filled = DesignSystemImage(name: "ic_download_1_filled")
+  static let ic_error_filled = DesignSystemImage(name: "ic_error_filled")
+  static let ic_heart_filled = DesignSystemImage(name: "ic_heart_filled")
+  static let ic_home_filled = DesignSystemImage(name: "ic_home_filled")
+  static let ic_image_1_filled = DesignSystemImage(name: "ic_image_1_filled")
+  static let ic_image_2_filled = DesignSystemImage(name: "ic_image_2_filled")
+  static let ic_info_filled = DesignSystemImage(name: "ic_info_filled")
+  static let ic_lens_filled = DesignSystemImage(name: "ic_lens_filled")
+  static let ic_location_filled = DesignSystemImage(name: "ic_location_filled")
+  static let ic_message_1_filled = DesignSystemImage(name: "ic_message_1_filled")
+  static let ic_message_2_filled = DesignSystemImage(name: "ic_message_2_filled")
+  static let ic_minuscircle_filled = DesignSystemImage(name: "ic_minuscircle_filled")
+  static let ic_money_JPY_filled = DesignSystemImage(name: "ic_money_JPY_filled")
+  static let ic_money_KR_filled = DesignSystemImage(name: "ic_money_KR_filled")
+  static let ic_money_USA_filled = DesignSystemImage(name: "ic_money_USA_filled")
+  static let ic_movelist_filled = DesignSystemImage(name: "ic_movelist_filled")
+  static let ic_notice_filled = DesignSystemImage(name: "ic_notice_filled")
+  static let ic_notify_filled = DesignSystemImage(name: "ic_notify_filled")
+  static let ic_orderlist_filled = DesignSystemImage(name: "ic_orderlist_filled")
+  static let ic_pen_filled = DesignSystemImage(name: "ic_pen_filled")
+  static let ic_phone_filled = DesignSystemImage(name: "ic_phone_filled")
+  static let ic_pluscircle_filled = DesignSystemImage(name: "ic_pluscircle_filled")
+  static let ic_profile_filled = DesignSystemImage(name: "ic_profile_filled")
+  static let ic_refresh_2_filled = DesignSystemImage(name: "ic_refresh_2_filled")
+  static let ic_search_filled = DesignSystemImage(name: "ic_search_filled")
+  static let ic_speechbubble_filled = DesignSystemImage(name: "ic_speechbubble_filled")
+  static let ic_ssmk_filled = DesignSystemImage(name: "ic_ssmk_filled")
+  static let ic_star_filled = DesignSystemImage(name: "ic_star_filled")
+  static let ic_stockanswer_filled = DesignSystemImage(name: "ic_stockanswer_filled")
+  static let ic_stockcheck_filled = DesignSystemImage(name: "ic_stockcheck_filled")
+  static let ic_store_1_filled = DesignSystemImage(name: "ic_store_1_filled")
+  static let ic_store_2_filled = DesignSystemImage(name: "ic_store_2_filled")
+  static let ic_talk_filled = DesignSystemImage(name: "ic_talk_filled")
+  static let ic_thumbs_down_filled = DesignSystemImage(name: "ic_thumbs_down_filled")
+  static let ic_thumbs_up_filled = DesignSystemImage(name: "ic_thumbs_up_filled")
+  static let ic_time_filled = DesignSystemImage(name: "ic_time_filled")
+  static let ic_trash_filled = DesignSystemImage(name: "ic_trash_filled")
+  static let ic_x_circle_filled = DesignSystemImage(name: "ic_x_circle_filled")
+  static let ic_x_circle_s_filled = DesignSystemImage(name: "ic_x_circle_s_filled")
+  static let ic_2gridview = DesignSystemImage(name: "ic_2gridview")
+  static let ic_3gridview = DesignSystemImage(name: "ic_3gridview")
+  static let ic_accountnumber_account = DesignSystemImage(name: "ic_accountnumber_account")
+  static let ic_accountnumber_bank = DesignSystemImage(name: "ic_accountnumber_bank")
+  static let ic_adcenter = DesignSystemImage(name: "ic_adcenter")
+  static let ic_addaccount = DesignSystemImage(name: "ic_addaccount")
+  static let ic_alarm = DesignSystemImage(name: "ic_alarm")
+  static let ic_alarm_ad = DesignSystemImage(name: "ic_alarm_ad")
+  static let ic_alarm_announce = DesignSystemImage(name: "ic_alarm_announce")
+  static let ic_alarm_etc = DesignSystemImage(name: "ic_alarm_etc")
+  static let ic_alarm_order = DesignSystemImage(name: "ic_alarm_order")
+  static let ic_alarm_request = DesignSystemImage(name: "ic_alarm_request")
+  static let ic_arrow_close = DesignSystemImage(name: "ic_arrow_close")
+  static let ic_arrow_left = DesignSystemImage(name: "ic_arrow_left")
+  static let ic_arrow_open = DesignSystemImage(name: "ic_arrow_open")
+  static let ic_arrow_right = DesignSystemImage(name: "ic_arrow_right")
+  static let ic_arrow_up_left = DesignSystemImage(name: "ic_arrow_up_left")
+  static let ic_arrowhead = DesignSystemImage(name: "ic_arrowhead")
+  static let ic_big_speechbubble = DesignSystemImage(name: "ic_big_speechbubble")
+  static let ic_bookmark1 = DesignSystemImage(name: "ic_bookmark1")
+  static let ic_bookmark_1 = DesignSystemImage(name: "ic_bookmark_1")
+  static let ic_bookmark_2 = DesignSystemImage(name: "ic_bookmark_2")
+  static let ic_calendar = DesignSystemImage(name: "ic_calendar")
+  static let ic_camera = DesignSystemImage(name: "ic_camera")
+  static let ic_cart = DesignSystemImage(name: "ic_cart")
+  static let ic_cash = DesignSystemImage(name: "ic_cash")
+  static let ic_category = DesignSystemImage(name: "ic_category")
+  static let ic_category_1 = DesignSystemImage(name: "ic_category_1")
+  static let ic_certificate = DesignSystemImage(name: "ic_certificate")
+  static let ic_check = DesignSystemImage(name: "ic_check")
+  static let ic_check_s = DesignSystemImage(name: "ic_check_s")
+  static let ic_checkcircle = DesignSystemImage(name: "ic_checkcircle")
+  static let ic_comment = DesignSystemImage(name: "ic_comment")
+  static let ic_controller = DesignSystemImage(name: "ic_controller")
+  static let ic_coupon = DesignSystemImage(name: "ic_coupon")
+  static let ic_cshelp = DesignSystemImage(name: "ic_cshelp")
+  static let ic_deliverybox = DesignSystemImage(name: "ic_deliverybox")
+  static let ic_download_1 = DesignSystemImage(name: "ic_download_1")
+  static let ic_download_2 = DesignSystemImage(name: "ic_download_2")
+  static let ic_edit_line = DesignSystemImage(name: "ic_edit_line")
+  static let ic_error = DesignSystemImage(name: "ic_error")
+  static let ic_file_copy = DesignSystemImage(name: "ic_file_copy")
+  static let ic_file_text = DesignSystemImage(name: "ic_file_text")
+  static let ic_filter = DesignSystemImage(name: "ic_filter")
+  static let ic_flash_auto = DesignSystemImage(name: "ic_flash_auto")
+  static let ic_flash_off = DesignSystemImage(name: "ic_flash_off")
+  static let ic_flash_on = DesignSystemImage(name: "ic_flash_on")
+  static let ic_folder = DesignSystemImage(name: "ic_folder")
+  static let ic_folder_add = DesignSystemImage(name: "ic_folder_add")
+  static let ic_folder_move = DesignSystemImage(name: "ic_folder_move")
+  static let ic_frequentchat = DesignSystemImage(name: "ic_frequentchat")
+  static let ic_global = DesignSystemImage(name: "ic_global")
+  static let ic_hashtag = DesignSystemImage(name: "ic_hashtag")
+  static let ic_heart = DesignSystemImage(name: "ic_heart")
+  static let ic_history = DesignSystemImage(name: "ic_history")
+  static let ic_home = DesignSystemImage(name: "ic_home")
+  static let ic_image_1 = DesignSystemImage(name: "ic_image 1")
+  static let ic_image = DesignSystemImage(name: "ic_image")
+  static let ic_info_1 = DesignSystemImage(name: "ic_info 1")
+  static let ic_info = DesignSystemImage(name: "ic_info")
+  static let ic_lens = DesignSystemImage(name: "ic_lens")
+  static let ic_list_sort = DesignSystemImage(name: "ic_list_sort")
+  static let ic_listorder = DesignSystemImage(name: "ic_listorder")
+  static let ic_listview = DesignSystemImage(name: "ic_listview")
+  static let ic_loading = DesignSystemImage(name: "ic_loading")
+  static let ic_location = DesignSystemImage(name: "ic_location")
+  static let ic_manage = DesignSystemImage(name: "ic_manage")
+  static let ic_membership = DesignSystemImage(name: "ic_membership")
+  static let ic_memo = DesignSystemImage(name: "ic_memo")
+  static let ic_message_1 = DesignSystemImage(name: "ic_message_1")
+  static let ic_minus = DesignSystemImage(name: "ic_minus")
+  static let ic_minuscircle = DesignSystemImage(name: "ic_minuscircle")
+  static let ic_money_JPY = DesignSystemImage(name: "ic_money_JPY")
+  static let ic_money_KR = DesignSystemImage(name: "ic_money_KR")
+  static let ic_money_USA = DesignSystemImage(name: "ic_money_USA")
+  static let ic_more_horiz = DesignSystemImage(name: "ic_more_horiz")
+  static let ic_more_vert = DesignSystemImage(name: "ic_more_vert")
+  static let ic_myaccount = DesignSystemImage(name: "ic_myaccount")
+  static let ic_mypage_editimage = DesignSystemImage(name: "ic_mypage_editimage")
+  static let ic_new_l = DesignSystemImage(name: "ic_new_l")
+  static let ic_new_m = DesignSystemImage(name: "ic_new_m")
+  static let ic_new_s = DesignSystemImage(name: "ic_new_s")
+  static let ic_notice = DesignSystemImage(name: "ic_notice")
+  static let ic_notify = DesignSystemImage(name: "ic_notify")
+  static let ic_orderdetail_ac_copy = DesignSystemImage(name: "ic_orderdetail_ac_copy")
+  static let ic_orderlist = DesignSystemImage(name: "ic_orderlist")
+  static let ic_out = DesignSystemImage(name: "ic_out")
+  static let ic_outlink = DesignSystemImage(name: "ic_outlink")
+  static let ic_pen = DesignSystemImage(name: "ic_pen")
+  static let ic_phone = DesignSystemImage(name: "ic_phone")
+  static let ic_plus = DesignSystemImage(name: "ic_plus")
+  static let ic_plus_ad_forward = DesignSystemImage(name: "ic_plus_ad_forward")
+  static let ic_pluscircle = DesignSystemImage(name: "ic_pluscircle")
+  static let ic_preview_no = DesignSystemImage(name: "ic_preview_no")
+  static let ic_preview_yes = DesignSystemImage(name: "ic_preview_yes")
+  static let ic_profile = DesignSystemImage(name: "ic_profile")
+  static let ic_recentproduct = DesignSystemImage(name: "ic_recentproduct")
+  static let ic_refresh_1 = DesignSystemImage(name: "ic_refresh_1")
+  static let ic_refresh_2 = DesignSystemImage(name: "ic_refresh_2")
+  static let ic_register = DesignSystemImage(name: "ic_register")
+  static let ic_repeat = DesignSystemImage(name: "ic_repeat")
+  static let ic_report = DesignSystemImage(name: "ic_report")
+  static let ic_restocked = DesignSystemImage(name: "ic_restocked")
+  static let ic_restore = DesignSystemImage(name: "ic_restore")
+  static let ic_scash = DesignSystemImage(name: "ic_scash")
+  static let ic_search = DesignSystemImage(name: "ic_search")
+  static let ic_setting = DesignSystemImage(name: "ic_setting")
+  static let ic_share_1 = DesignSystemImage(name: "ic_share_1")
+  static let ic_share_2 = DesignSystemImage(name: "ic_share_2")
+  static let ic_share_3 = DesignSystemImage(name: "ic_share_3")
+  static let ic_sinsanglens = DesignSystemImage(name: "ic_sinsanglens")
+  static let ic_smartphone = DesignSystemImage(name: "ic_smartphone")
+  static let ic_soldout = DesignSystemImage(name: "ic_soldout")
+  static let ic_spay = DesignSystemImage(name: "ic_spay")
+  static let ic_speechbubble = DesignSystemImage(name: "ic_speechbubble")
+  static let ic_ssmk = DesignSystemImage(name: "ic_ssmk")
+  static let ic_star = DesignSystemImage(name: "ic_star")
+  static let ic_star_filled_32_ver01 = DesignSystemImage(name: "ic_star_filled_32_ver01")
+  static let ic_star_line_32_ver01 = DesignSystemImage(name: "ic_star_line_32_ver01")
+  static let ic_store_1 = DesignSystemImage(name: "ic_store_1")
+  static let ic_store_2 = DesignSystemImage(name: "ic_store_2")
+  static let ic_store_3 = DesignSystemImage(name: "ic_store_3")
+  static let ic_store_3_filled = DesignSystemImage(name: "ic_store_3_filled")
+  static let ic_talk = DesignSystemImage(name: "ic_talk")
+  static let ic_thumbs_down = DesignSystemImage(name: "ic_thumbs_down")
+  static let ic_thumbs_up = DesignSystemImage(name: "ic_thumbs_up")
+  static let ic_time = DesignSystemImage(name: "ic_time")
+  static let ic_trash = DesignSystemImage(name: "ic_trash")
+  static let ic_up = DesignSystemImage(name: "ic_up")
+  static let ic_up_1_filled = DesignSystemImage(name: "ic_up_1_filled")
+  static let ic_waitaccount = DesignSystemImage(name: "ic_waitaccount")
+  static let ic_wallet = DesignSystemImage(name: "ic_wallet")
+  static let ic_website = DesignSystemImage(name: "ic_website")
+  static let ic_x = DesignSystemImage(name: "ic_x")
+  static let ic_x_circle = DesignSystemImage(name: "ic_x_circle")
+  static let ic_x_s = DesignSystemImage(name: "ic_x_s")
+  static let ic_ad = DesignSystemImage(name: "ic_ad")
+  static let ic_calculate = DesignSystemImage(name: "ic_calculate")
+  static let ic_delivery = DesignSystemImage(name: "ic_delivery")
+  static let ic_studio = DesignSystemImage(name: "ic_studio")
+}
+// swiftlint:enable identifier_name line_length nesting type_body_length type_name
+
+// MARK: - Implementation Details
+
+internal struct DesignSystemImage {
+  internal fileprivate(set) var name: String
+
+  internal typealias UIKitImage = UIImage
+
+  internal var image: UIKitImage {
+    let bundle = BundleToken.bundle
+    let image = UIKitImage(named: name, in: bundle, compatibleWith: nil)
+    guard let result = image else {
+      fatalError("Unable to load image asset named \(name).")
+    }
+    return result
+  }
+
+  internal func image(compatibleWith traitCollection: UITraitCollection) -> UIKitImage {
+    let bundle = BundleToken.bundle
+    guard let result = UIKitImage(named: name, in: bundle, compatibleWith: traitCollection) else {
+      fatalError("Unable to load image asset named \(name).")
+    }
+    return result
+  }
+
+  #if canImport(SwiftUI)
+  internal var swiftUIImage: SwiftUI.Image {
+    SwiftUI.Image(asset: self)
+  }
+  #endif
+}
+
+internal extension DesignSystemImage.UIKitImage {
+  convenience init?(asset: DesignSystemImage) {
+    let bundle = BundleToken.bundle
+    self.init(named: asset.name, in: bundle, compatibleWith: nil)
+  }
+}
+
+#if canImport(SwiftUI)
+internal extension SwiftUI.Image {
+  init(asset: DesignSystemImage) {
+    let bundle = BundleToken.bundle
+    self.init(asset.name, bundle: bundle)
+  }
+
+  init(asset: DesignSystemImage, label: Text) {
+    let bundle = BundleToken.bundle
+    self.init(asset.name, bundle: bundle, label: label)
+  }
+
+  init(decorative asset: DesignSystemImage) {
+    let bundle = BundleToken.bundle
+    self.init(decorative: asset.name, bundle: bundle)
+  }
+}
+#endif
+
+// swiftlint:disable convenience_type
+private final class BundleToken {
+  static let bundle: Bundle = {
+    #if SWIFT_PACKAGE
+    return Bundle.module
+    #else
+    return Bundle(for: BundleToken.self)
+    #endif
+  }()
+}
+// swiftlint:enable convenience_type
+

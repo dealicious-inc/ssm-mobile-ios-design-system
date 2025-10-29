7b5a5454caff06fd0c4a1e3e2be996d8f78d69cf
jalynneyoon <jalynneyoon@gmail.com>
2025-10-29 11:08:34 +0900

    [MP-6159] Color static 변수 추가

---
+++ Sources/DealiDesignKit/Color/Color+SystemColor.swift
@@ -0,0 +1,79 @@
+//
+//  Color+SystemColor.swift
+//  DealiDesignKit
+//
+//  Created by 윤조현 on 10/29/25.
+//
+
+import SwiftUI
+
+// MARK: - Primary
+public extension Color {
+    static var primary01: Color { UIColor.primary01.asColor() }
+    static var primary02: Color { UIColor.primary02.asColor() }
+    static var primary03: Color { UIColor.primary03.asColor() }
+    static var primary04: Color { UIColor.primary04.asColor() }
+    static var primary05: Color { UIColor.primary05.asColor() }
+    static var primary06: Color { UIColor.primary06.asColor() }
+    static var primary07: Color { UIColor.primary07.asColor() }
+}
+
+// MARK: - Secondary
+public extension Color {
+    static var secondary01: Color { UIColor.secondary01.asColor() }
+    static var secondary02: Color { UIColor.secondary02.asColor() }
+    static var secondary03: Color { UIColor.secondary03.asColor() }
+    static var secondary04: Color { UIColor.secondary04.asColor() }
+    static var secondary05: Color { UIColor.secondary05.asColor() }
+    static var secondary06: Color { UIColor.secondary06.asColor() }
+}
+
+// MARK: - Gray
+public extension Color {
+    static var g05: Color { UIColor.g05.asColor() }
+    static var g10: Color { UIColor.g10.asColor() }
+    static var g20: Color { UIColor.g20.asColor() }
+    static var g30: Color { UIColor.g30.asColor() }
+    static var g40: Color { UIColor.g40.asColor() }
+    static var g50: Color { UIColor.g50.asColor() }
+    static var g60: Color { UIColor.g60.asColor() }
+    static var g70: Color { UIColor.g70.asColor() }
+    static var g80: Color { UIColor.g80.asColor() }
+    static var g100: Color { UIColor.g100.asColor() }
+}
+
+// MARK: - Service
+public extension Color {
+    static var error: Color { UIColor.error.asColor() }
+    static var warning: Color { UIColor.warning.asColor() }
+    static var linkAndInfo: Color { UIColor.linkAndInfo.asColor() }
+    static var success: Color { UIColor.success.asColor() }
+}
+
+// MARK: - ETC
+public extension Color {
+    static var b70: Color { UIColor.b70.asColor() }
+    static var b60: Color { UIColor.b60.asColor() }
+    static var b50: Color { UIColor.b50.asColor() }
+    static var b40: Color { UIColor.b40.asColor() }
+    static var b30: Color { UIColor.b30.asColor() }
+    static var b20: Color { UIColor.b20.asColor() }
+    static var b10: Color { UIColor.b10.asColor() }
+    static var b5: Color { UIColor.b5.asColor() }
+    static var b2: Color { UIColor.b2.asColor() }
+    static var w90: Color { UIColor.w90.asColor() }
+    static var w70: Color { UIColor.w70.asColor() }
+    static var w50: Color { UIColor.w50.asColor() }
+    static var w30: Color { UIColor.w30.asColor() }
+    static var w20: Color { UIColor.w20.asColor() }
+    static var w15: Color { UIColor.w15.asColor() }
+    static var w10: Color { UIColor.w10.asColor() }
+    static var w5: Color { UIColor.w5.asColor() }
+}
+
+// MARK: - 소매 멤버십
+public extension Color {
+    static var mbs01: Color { UIColor.mbs01.asColor() }
+    static var mbs02: Color { UIColor.mbs02.asColor() }
+}
+
